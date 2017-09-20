450d084ee6ad91f0ca6e80f2ef0506aa815e6890
Dmitry Ten <candalistic@gmail.com>
2017-09-19 21:20:16 -0400

    Moved index.html to the top folder for github.io. Updated paths.

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -5,16 +5,15 @@ var myButton = document.querySelector('button').onclick = function(){
 
 //define an array of 3 images, that user will be able to go through
 //for now, all these images are defined explicitly
-var imgPath1 = 'images/img-me.jpg';
-var imgPath2 = 'images/img-toronto.jpg';
-var imgPath3 = 'images/img-school.jpg';
-
+var imgPath1 = 'frontend/images/img-me.jpg';
+var imgPath2 = 'frontend/images/img-toronto.jpg';
+var imgPath3 = 'frontend/images/img-school.jpg';
 var images = [imgPath1, imgPath2, imgPath3]; 
 var imgIndex = 0;
+
 var img = document.getElementById('img-view');
 
 var prevButton = document.getElementById('prev-btn');
-
 prevButton.onclick = function() {
     if(imgIndex > 0) {
         //buttonOff(nextButton, false);
